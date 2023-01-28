e56a03990f7739f2a235b9bbff049522ee1be72e
rishi <rishi.veerepalli@gmail.com>
2023-01-28 10:14:34 -0800

    client

---
+++ movement/mavros_client.py
@@ -0,0 +1,78 @@
+# $ rosrun mavros mavparam set SYSID_MYGCS 1
+
+import rospy
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+from mavros_msgs.msg import OverrideRCIn, State
+import threading
+import time
+
+rc_pub = None
+rate = None
+state_sub = None
+current_state = State()
+mode = "MANUAL"
+
+channels = [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500]
+
+def state_cb(msg):
+    global current_state
+    current_state = msg
+
+def init_ros():
+    global rc_pub
+    global rate
+    global state_sub
+
+    rospy.init_node("mavros_example_client")
+
+    state_sub = rospy.Subscriber("mavros/state", State, callback = state_cb)
+    rc_pub = rospy.Publisher("mavros/rc/override", OverrideRCIn, queue_size=10)
+                                 
+    rate = rospy.Rate(20)
+
+def connect_arm():
+    rospy.wait_for_service("/mavros/cmd/arming")
+    arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)    
+    rospy.wait_for_service("/mavros/set_mode")
+    set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
+    
+    while(not rospy.is_shutdown() and not current_state.connected):
+        rate.sleep()
+
+    stab_set_mode = SetModeRequest()
+    stab_set_mode.custom_mode = mode
+    arm_cmd = CommandBoolRequest()
+    arm_cmd.value = True
+
+def send_rc():
+    msg = OverrideRCIn()
+    last_req = rospy.Time.now()
+
+    while(not rospy.is_shutdown()):
+        if(current_state.mode != mode and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+            if(set_mode_client.call(stab_set_mode).mode_sent == True):
+                rospy.loginfo("mode enabled")
+            
+            last_req = rospy.Time.now()
+        else:
+            if(not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+                if(arming_client.call(arm_cmd).success == True):
+                    rospy.loginfo("Vehicle armed")
+            
+                last_req = rospy.Time.now()
+
+        msg.channels = channels
+        rc_pub.publish(msg)
+
+        
+def main():
+    init_ros()
+    connect_arm()
+
+    thread = threading.Thread(target=send_rc)
+    thread.start()
+
+    # set elements of channels
+    
+if __name__ == "__main__":
+    main()
