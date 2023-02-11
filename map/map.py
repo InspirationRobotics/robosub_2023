bb3e929222fc989f0b377ec3f5a5aa18def791ee
rishi <rishi.veerepalli@gmail.com>
2023-02-11 06:50:52 -0800

    mission & map stuff

---
+++ map/map.py
@@ -0,0 +1,17 @@
+
+import math
+import translation
+import rospy
+import map_operations
+
+mapgrid = {
+    "orientation": 0,
+    "center": [0, 0],
+    "data": []
+}
+    
+def threshold_map(p):
+    pass
+
+def main():
+    pass
