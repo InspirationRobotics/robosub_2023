ad81b45510b7969a253fb6bad2876e3df9fd8f34
Mabel Szeto <mabeltreeszeto@gmail.com>
2023-04-01 18:56:21 -0700

    Update seaperch.py
    adding comments to help troubleshoot

--- seaperch.py
+++ seaperch.py
@@ -10,8 +10,8 @@ rc.setmode('ALT_HOLD')
 
 rc.arm()
 
-rc.throttle("time",1, -0.40)
-rc.throttle("time",1, -0.40)
+rc.throttle("time",1, -0.40) # this is to control depth, was going up instead of down
+rc.throttle("time",1, -0.40) # inside the parentheses set the meaurement to time, the time value in seconds, and the power
 
 rc.forward("time", 3, 0.30)
 rc.forward("time", 3, -0.30)
