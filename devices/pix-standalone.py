d153fe8690f12a1d00305a7c4a8d643531644581
rishi <rishi.veerepalli@gmail.com>
2023-04-27 17:28:17 -0700

    ros types

--- devices/pix-standalone.py
+++ devices/pix-standalone.py
@@ -6,7 +6,7 @@ import rospy
 from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
 from mavros_msgs.msg import OverrideRCIn, State
 from sensor_msgs.msg import Imu, FluidPressure
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Int32MultiArray
 import threading
 import time
 
