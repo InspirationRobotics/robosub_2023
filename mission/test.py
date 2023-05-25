9863ceab183edba87a166f2a6c3e6bbde69f1938
Parth Saxena <50534120+parth-2007@users.noreply.github.com>
2023-05-25 15:38:16 -0700

    Update test.py

--- mission/test.py
+++ mission/test.py
@@ -1,35 +1,5 @@
-# really simple baseline mission
-import rospy
-from std_msgs.msg import Int32MultiArray
-#from insp_msgs.msg import PointArray
+from motion.robot_control import RobotControl
 
-
-def get_pub(n, p):
-        return p[n][2]
-
-publishers = {
-        "raw": ["/auv/motion/raw", Int32MultiArray, None]
-}
-
-def pwm_cb(p): 
-#	if sd['mode'] == "raw":
-        get_pub("thrusters", publishers).publish(p)
-
-def init_ros_io(p, s):
-        for i in s.values():
-                i[3] = rospy.Subscriber(i[0], i[1], i[2], queue_size=10)
-                
-        for i in p.values():
-                i[2] = rospy.Publisher(i[0], i[1], queue_size=10)
-
-def SDoF(th, fw, lat, yaw, pitch, roll):
-    get_pub("raw", publishers).publish([1500, 1500, th, yaw, fw, pitch, roll, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500])
-                
-def main():
-    rospy.init_node("path_v1", anonymous=True)
-
-    while not rospy.is_shutdown():
-        SDof(1500, 1550, 1500, 1500, 1500, 1500)
-        sleep(1)
-    print("finished executing square mission")
-    rospy.spin()
+rc = RobotControl()
+rc.movement(0, 2, 0, 0, 0, 0, 2)
+print("finished")
