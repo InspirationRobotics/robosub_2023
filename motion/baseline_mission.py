41ba630326c41421ca617e51920d6c4635b17d3c
Ubuntu <saxenaparth007@gmail.com>
2023-05-25 16:00:30 -0700

    updated robot contrl lib

--- motion/baseline_mission.py
+++ motion/baseline_mission.py
@@ -1,5 +1,5 @@
 from robot_control import RobotControl
 
 rc = RobotControl()
-rc.movement(0, 2, 0, 0, 0, 0, 2)
+rc.movement(throttle=0, forward=2, lateral=0, yaw=0, pitch=0, roll=0, t=2)
 print("finished")
--- motion/robot_control.py
+++ motion/robot_control.py
@@ -14,21 +14,19 @@ class RobotControl:
         self.channels = [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500]
         print("RC Lib Initialized")
         
-    def movement(self, throttle, forward, lateral, yaw, pitch, roll, t):
+    def movement(self, **array):
         pwm = mavros_msgs.msg.OverrideRCIn()
         rate = rospy.Rate(5)
         channels = self.channels
-        channels[2] = ((throttle*80) + 1500)
-        channels[4] = ((forward*80) + 1500)
-        channels[5] = ((lateral*80) + 1500)
-        channels[3] = ((yaw*80) + 1500)
-        channels[6] = ((pitch*80) + 1500)
-        channels[7] = ((roll*80) + 1500)
+        channels[2] = ((array["throttle"]*80) + 1500)
+        channels[4] = ((array["forward"]*80) + 1500)
+        channels[5] = ((array["lateral"]*80) + 1500)
+        channels[3] = ((array["yaw"]*80) + 1500)
+        channels[6] = ((array["pitch"]*80) + 1500)
+        channels[7] = ((array["roll"]*80) + 1500)
         pwm.channels = channels
         rate.sleep()
         self.pub.publish(pwm)
-        time.sleep(t)
+        time.sleep(array["t"])
         pwm.channels = self.channels
         self.pub.publish(pwm)
-        
-        
