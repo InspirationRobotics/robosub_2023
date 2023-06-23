88354ea33610cdabfc4a01caf3c3cee41e3efcce
inspiration <eeshvij@gmail.com>
2023-06-22 22:50:56 -0700

    made wifi a bit smarter to reduce disconnects for grey

--- wifiGrey.sh
+++ wifiGrey.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-sudo nmcli d wifi connect TeamInspiration password Roboticsiscool!
+while true 
+do
+    if ping -c 2 -W 1 8.8.8.8 | grep -q '64 bytes'; then
+        echo "Connected"
+        sleep 5
+    else
+        echo "Disconnected"
+        sleep 2
+        sudo nmcli d wifi connect TeamInspiration password Roboticsiscool!
+        sleep 10
+    fi
+done
+
 #sleep 2
 #screen -dmS wifi ping -D 8.8.8.8
