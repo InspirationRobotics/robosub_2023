1d612206d6e66ac959f9279fbc9acc2260fbc34d
Mabel Szeto <mabeltreeszeto@gmail.com>
2023-07-02 18:09:56 -0700

    Update coingatestyle.py
    added in the torpedo code

--- motion/coingatestyle.py
+++ motion/coingatestyle.py
@@ -6,7 +6,7 @@ from servos import *
 # For july 3rd pool test, written on 7/1/2023 and 7/2/2023
 # Coin toss, gate, and style points with working computer vision, autonomously fire torpedoes and drop the markers
 
-start = 331 # this is the heading the robot will align to at the beginning of the program, need to update when go to the pool
+gate = 331 # this is the heading the robot will align to at the beginning of the program, need to update when go to the pool
 torpedo1 = True # default torpedoes values are either loaded or unloaded
 torpedo2 = True
 marker1 = True # default markers values are either loaded or unloaded
@@ -17,11 +17,11 @@ rc = RobotControl()
 time.sleep(1)
 rc.setDepth(0.65) #setting depth
 time.sleep(4)
-rc.setHeading(start) 
+rc.setHeading(gate) 
 
 # move towards the gate
 rc.forwardDist(2, 2) 
-# for the monday pool test use (5,2) goes forward for 5 seconds at power of 2 which is about 
+# for the monday pool test use (5,2) goes forward for 5 seconds at power of 2 which is about 13 meters
 time.sleep(2)
 
 # insert the cv code here 
@@ -32,12 +32,18 @@ rc.forwardDist(2, 2)
 # style points, yaw 720 degrees
 rc.setHeading(start+720) 
 
-# add in torpedo shooter - shoot one, pause, change depth, pause, shoot the second
-torpedoLauncher(1) # shoots first torpedo
-time.sleep(1)
-torpedoLauncher(2) # shoots second torpedo
-time.sleep(1)
-# move forward
+# torpedo shooter - shoot one, pause, change depth, pause, shoot the second
+rc.forwardDist(2, 2) 
+if torpedo1 and torpedo2 == True:
+  torpedoLauncher(1) # shoots first torpedo
+  torpedo1 = False  
+  time.sleep(1)
+  rc.setDepth(0.45) # change depth, moving up .5 meters, not sure if this is the right way to do it 
+  torpedoLauncher(2) # shoots second torpedo
+  torpedo2 = False
+  time.sleep(1)
+else:
+  rc.forwardDist(1, 2)  
 
 # drop both markers
 dropper(1) # drops first marker
