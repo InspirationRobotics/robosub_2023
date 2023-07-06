4db51f9429dc1be093c99dd8809c1def64aad322
Dat-Bois <eesh.vij@gmail.com>
2023-07-06 16:57:40 -0700

    added auto error clearing for polulu

--- scripts/mavros.sh
+++ scripts/mavros.sh
@@ -4,6 +4,9 @@ PRODUCT=$(sudo lshw -json | jq '.product') || PRODUCT=$(sudo lshw -json | jq '.[
 
 if [[ $PRODUCT == *"Xavier"* ]]; then
   echo "Detected $PRODUCT setting to Xavier init"
+  POLULU=$(/usr/bin/python3 /home/inspiration/auv/scripts/deviceHelper.py platform-3610000.xhci-usb-0:2.1.1:1.0)
+  echo "Found Polulu Servo driver at $POLULU"
+  screen -dmS polulu bash -c "sudo bash /home/inspiration/auv/maestro-linux/clearPoluluErrors.sh $POLULU"
   DISTRO="noetic"
 fi
 if [[ $PRODUCT == *"Nano"* ]]; then
