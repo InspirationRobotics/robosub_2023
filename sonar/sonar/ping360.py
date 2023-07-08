027bd6055e38e131110c33c581f67bb8a836a743
Maxime Ellerbach <maxime@ellerbach.net>
2023-07-07 17:46:40 -0700

    hot fix ping360 sector

--- sonar/sonar/ping360.py
+++ sonar/sonar/ping360.py
@@ -18,7 +18,7 @@ class Ping360(brping.Ping360):
         self._scan_mode = self.set_scan_mode(scan_mode)
         self._angle_range = self.set_angle_range(angle_range)
         self._angle_step = self.set_angle_step(angle_step)
-        self._angle = angle_range[0]
+        self._angle = angle_range[0] % 400
         self._increment = self._angle_step
 
         logger.debug(f"Ping360 initialized")
@@ -38,7 +38,7 @@ class Ping360(brping.Ping360):
             raise ValueError(f"invalid angle range: {angle_range}")
 
         self._angle_range = angle_range
-        self._angle = angle_range[0]
+        self._angle = angle_range[0] % 400
         return self._angle_range
 
     def set_angle_step(self, angle_step):
