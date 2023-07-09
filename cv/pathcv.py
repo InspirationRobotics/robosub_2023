1c800ecbe7abbad081da1299c81dcae9701c3c85
Dat-Bois <eeshvij@gmail.com>
2023-07-08 19:42:12 -0700

    updating pathcv

--- cv/pathcv.py
+++ cv/pathcv.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import time
 import sys
+import signal
 #from robot_control import RobotControl
 import rospy
 from sensor_msgs.msg import Image
@@ -9,7 +10,7 @@ from cv_bridge import CvBridge, CvBridgeError
 
 # initializing publisher, will output cv image here
 br = CvBridge()
-pubForward = rospy.Publisher('/auv/camera/videoOutput0', Image,queue_size=10)
+pubForward = rospy.Publisher('/auv/camera/videoUSBOutput1', Image,queue_size=10)
 global forwardVideo
 forwardVideo = None
 rospy.init_node("CV", anonymous=True)
@@ -17,25 +18,34 @@ rospy.Rate(30)
 
 # initializing subscriber and callback to retrieve video feed, assigning to forwardVideo 
 def callbackForward(msg):
-        global forwardVideo
-        try:
-            forwardVideo = br.imgmsg_to_cv2(msg)
-        except Exception as e:
-            print("Forward Output Error, make sure running in Python2")
-            print(e)
+    global forwardVideo
+    try:
+        forwardVideo = br.imgmsg_to_cv2(msg)
+    except Exception as e:
+        print("Forward Output Error, make sure running in Python2")
+        print(e)
 
-rospy.Subscriber("/auv/camera/videoRaw0",Image,callbackForward) # subscribing here
+def onExit(signum, frame):
+    try:
+        print("\nDisarming and exiting...")
+        rospy.signal_shutdown("Rospy Exited")
+        exit(1)
+    except:
+        pass
+
+rospy.Subscriber("/auv/camera/videoUSBRaw1",Image,callbackForward) # subscribing here
 
 width = 640 # width of frame
 height = 480 # height of frame
+kill = True
 
-while 1:
+while not rospy.is_shutdown():
     try:
         frame = forwardVideo
         into_hsv =(cv2.cvtColor(frame,cv2.COLOR_BGR2HSV))
         L_limit=np.array([8, 100, 100]) 
         U_limit=np.array([50, 255, 255]) 
-    
+        signal.signal(signal.SIGINT, onExit)
         # L_limit=np.array([5, 25, 50]) 
         # U_limit=np.array([30, 255, 255]) 
     
@@ -61,7 +71,7 @@ while 1:
         hline = cv2.line(frame, (640, 0), (0, 0), (0, 255, 255), 8)
         if contours:
             c = max(contours, key = cv2.contourArea)
-    
+            cv2.drawContours(frame, c, -1, 255, 3)
             M = cv2.moments(c)
             if M['m00'] != 0:
                 cx = int(M['m10']/M['m00'])
@@ -124,3 +134,5 @@ while 1:
     except Exception as e:
         print(e)
         pass
+
+
