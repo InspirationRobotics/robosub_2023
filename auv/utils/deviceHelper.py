d78556bb9f59d5956aa371c119d7977175aca865
Dat-Bois <eeshvij@gmail.com>
2023-07-12 23:13:39 -0700

    small fix

--- auv/utils/deviceHelper.py
+++ auv/utils/deviceHelper.py
@@ -85,6 +85,6 @@ def dataFromConfig(name):
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
-        print(dataFromConfig([sys.argv[1]]))
+        print(dataFromConfig(sys.argv[1]))
     else:
         print(dataFromConfig("pixhawk"))
