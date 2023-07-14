7ca6d71682010d2c8906db88227a094da7dc3259
Maxime Ellerbach <maxime@ellerbach.net>
2023-07-13 23:20:03 -0700

    make cvHandler support multi-scripts (threading)

--- auv/utils/cvHandler.py
+++ auv/utils/cvHandler.py
@@ -5,6 +5,7 @@ CV Handler
 import json
 import logging
 import os
+import threading
 import time
 
 import rospy
@@ -16,17 +17,16 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class CVHandler:
-    def __init__(self, **config):
-        """Init of CV Handler"""
+class ScriptHandler:
+    def __init__(self, file_name, cv_object, camera_topic):
+        self.cv_object = cv_object
+        self.camera_topic = camera_topic
+        self.file_name = file_name
 
-        # Create the cv bridge, the ROS node, the subscriber and the publishers
         self.br = CvBridge()
-        self.rospy = rospy.init_node("cv_handler", anonymous=True)
-        self.sub_cmd = rospy.Subscriber("auv/cv_handler/cmd", String, self.callback_cmd)
-        self.pub_out = rospy.Publisher("auv/cv_handler/output", String, queue_size=10)
-        self.sub_cv = None
-        self.pub_viz = None
+        self.sub_cv = rospy.Subscriber(self.camera_topic, Image, self.callback_cam)
+        self.pub_viz = rospy.Publisher(self.camera_topic.replace("Raw", "Output"), Image, queue_size=10)
+        self.pub_out = rospy.Publisher("auv/cv_handler/{}".format(file_name), String, queue_size=10)
 
         self.next_frame = None
         self.running = False
@@ -34,56 +34,8 @@ class CVHandler:
         self.last_received = time.time()
         self.last_processed = self.last_received
 
-    def init_ros(self, cv_class, camera_topic):
-        """Init the ROS part of the CV Handler"""
-        self.cv_object = cv_class()
-        self.camera_topic = camera_topic
-
-        self.sub_cv = rospy.Subscriber(self.camera_topic, Image, self.callback_cam)
-        self.pub_viz = rospy.Publisher(self.camera_topic.replace("Raw", "Output"), Image, queue_size=10)
-
-    def callback_cmd(self, msg):
-        """Callback for the cmd subscriber"""
-        try:
-            cmd = json.loads(msg.data)
-            module_name = cmd.get("module_name", None)
-            camera_topic = cmd.get("camera_topic", None)
-            stop = cmd.get("stop", False) # we just received a stop command
-
-            self.running = False
-            while not self.closed:
-                time.sleep(0.1)
-            if stop:
-                return
-
-            if module_name is None:
-                logger.error("No file name given")
-                return
-
-            if camera_topic is None:
-                logger.error("No camera topic given")
-                return
-
-            # Check if the file exists
-            if not os.path.isfile(module_name):
-                logger.error("File does not exist")
-                return
-
-
-            # Import the CV class from the module_name
-            # module name is as follows: auv.cv.module_name
-            module = __import__(module_name, fromlist=[""])
-            cv_class = getattr(module, "CV")
-
-            # Init the ROS part
-            self.init_ros(cv_class, camera_topic)
-
-            # Run the CV
-            self.run()
-
-        except Exception as e:
-            logger.error("Error while converting cmd to json")
-            logger.error(e)
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
 
     def callback_cam(self, msg):
         """Callback for the cam subscriber"""
@@ -94,9 +46,7 @@ class CVHandler:
             logger.error("Error while converting image to cv2")
             logger.error(e)
 
-    def run(self):
-        """Run the CV Handler"""
-        self.closed = False
+    def run(self, frame):
         self.running = True
 
         while self.running:
@@ -126,8 +76,73 @@ class CVHandler:
             if viz_img is not None:
                 self.pub_viz.publish(self.br.cv2_to_imgmsg(viz_img))
 
+    def stop(self):
+        self.running = False
+        self.thread.join()
+        
+        self.sub_cv.unregister()
+        self.pub_viz.unregister()
+        self.pub_out.unregister()
         self.closed = True
 
+    def __del__(self):
+        if not self.closed:
+            self.stop()
+
+
+class CVHandler:
+    def __init__(self, **config):
+        """Init of CV Handler"""
+        self.config = config
+
+        # Create the cv bridge, the ROS node, the subscriber and the publishers
+        self.rospy = rospy.init_node("cv_handler", anonymous=True)
+        self.sub_cmd = rospy.Subscriber("auv/cv_handler/cmd", String, self.callback_cmd)
+        
+        # stores the running cv scripts keyed by file name
+        self.active_cv_scripts = {}
+
+    def callback_cmd(self, msg):
+        """Callback for the cmd subscriber"""
+        try:
+            cmd = json.loads(msg.data)
+            file_name = cmd.get("file_name", None)
+            stop_file = cmd.get("stop", None)  # we just received a stop command
+
+            if stop and stop_file in self.active_cv_scripts:
+                self.active_cv_scripts[stop_file].stop()
+                del self.active_cv_scripts[stop_file]
+                return
+
+            elif stop:
+                logger.error("Cannot stop a script that is not running")
+
+            if file_name is None:
+                logger.error("No file name given")
+                return
+            
+            # Check if the file exists
+            if not os.path.isfile(file_name):
+                logger.error("File does not exist")
+                return
+
+            # Import the CV class from the file_name
+            # module name is as follows: auv.cv.file_name
+            module = __import__(file_name, fromlist=[""])
+            cv_class = getattr(module, "CV")
+
+            # Get the camera topic, if not specified, use the default front camera
+            camera_topic = getattr(cv_class, "camera", None)
+            if camera_topic is None:
+                logger.warning("No camera topic specified, using default front camera")
+                camera_topic = "/auv/camera/videoUSBRaw0"
+
+            # Init individual cv script handler
+            self.running[file_name] = ScriptHandler(cv_class(**self.config), camera_topic)
+
+        except Exception as e:
+            logger.error("Error while converting cmd to json")
+            logger.error(e)
 
 if __name__ == "__main__":
     # spinup the cv handler with the surfacing cv
@@ -135,5 +150,3 @@ if __name__ == "__main__":
     from auv.cv import surfacing_cv, template_cv
 
     cv = CVHandler()
-    cv.init_ros(surfacing_cv.CV, "/auv/camera/videoUSBRaw0")
-    cv.run()
