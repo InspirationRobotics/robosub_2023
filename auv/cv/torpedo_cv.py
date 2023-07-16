53cced687415acc54232aa679cc4c79e7855a02c
Maxime Ellerbach <maxime@ellerbach.net>
2023-07-16 15:55:23 -0700

    small fixes for torpedo

--- auv/cv/torpedo_cv.py
+++ auv/cv/torpedo_cv.py
@@ -31,14 +31,14 @@ class CV:
         self.lostSight = 0
         logger.info("Torpedo CV init")
 
-        self.target_center_x = 210      # Where we want the center of the circle
-        self.target_center_y = 350      # to be when close to mission
+        self.target_center_x = 210  # Where we want the center of the circle
+        self.target_center_y = 350  # to be when close to mission
 
-        self.center_x = 240             # True center of screen which will be
-        self.center_y = 320             # used to align the sub when far away
+        self.center_x = 240  # True center of screen which will be
+        self.center_y = 320  # used to align the sub when far away
 
-        self.threshold_far = 30         # Margin of error when far
-        self.threshold_near = 50        # "      "      " when near
+        self.threshold_far = 30  # Margin of error when far
+        self.threshold_near = 50  # "      "      " when near
         self.scan_timer = 0
         self.distance_to_target = 9999
         self.near = False
@@ -49,9 +49,8 @@ class CV:
         # TODO Fill with distance boundary that we will get from ping 360 to
         # switch from center the center of the sub the the largest circle, to
         # then centering the torpedo zone
-        self.far_near_boundary = 2    # if distance < 2 meteres, we are near
-        self.fire_distance = 280        # if distacne < 1 meter, we are in range
-
+        self.far_near_boundary = 2  # if distance < 2 meteres, we are near
+        self.fire_distance = 280  # if distacne < 1 meter, we are in range
 
         step_angle = 1
         max_range = 20
@@ -65,10 +64,9 @@ class CV:
             max_range=max_range,
             gain=2,
             transmit_freq=800,
-    )
+        )
 
-
-    def get_circles (self):
+    def get_circles(self, frame):
         """
         Returns the center and radius of the detected circle in the frame
         using a Hough circle detection on Canny edge detection
@@ -82,16 +80,17 @@ class CV:
         edges = cv2.GaussianBlur(edges, (5, 5), 2, 2)
         edges = cv2.GaussianBlur(edges, (5, 5), 2, 2)
 
-
         # Detecting Hough Circles on the image and saving them in circles
-        circles = cv2.HoughCircles(edges,
-                                    cv2.HOUGH_GRADIENT,
-                                    1, 
-                                    edges.shape[0] / 8, 
-                                    param1=10, 
-                                    param2=100, 
-                                    minRadius=1, maxRadius=600)
-        
+        circles = cv2.HoughCircles(
+            edges,
+            cv2.HOUGH_GRADIENT,
+            1,
+            edges.shape[0] / 8,
+            param1=10,
+            param2=100,
+            minRadius=1,
+            maxRadius=600,
+        )
 
         # Draw all circles on the original image
         if circles is not None:
@@ -99,20 +98,14 @@ class CV:
 
             circles = np.round(circles[0, :]).astype("int")
             for circle in circles:
-                
-                x,y = circles[i,0], circles[i,1]
+                x, y = circles[i, 0], circles[i, 1]
                 dist = circles[i, 2]
-                cv2.circle(self.frame, center=(x, y), radius=dist, 
-                           color=(0, 255, 0), thickness=20)
-                cv2.circle(self.frame, center=(circles[i, 0], circles[i, 1]), 
-                           radius=2, color=(0, 0, 255), thickness=2)
+                cv2.circle(self.frame, center=(x, y), radius=dist, color=(0, 255, 0), thickness=20)
+                cv2.circle(self.frame, center=(circles[i, 0], circles[i, 1]), radius=2, color=(0, 0, 255), thickness=2)
                 i += 1
-        
-        # Return list containing all circles in the frame
-        return (circles)
-                
-        
 
+        # Return list containing all circles in the frame
+        return circles
 
     def run(self, frame):
         """
@@ -120,175 +113,138 @@ class CV:
         This could be a loop, grabing frames using ROS, etc.
         """
         logging.info("Torpedo CV run")
-        
-        if(self.fired_torpedo_1 and self.fired_torpedo_2):
+
+        if self.fired_torpedo_1 and self.fired_torpedo_2:
             logging.info("Mission complete!!")
             return {
-                "lateral": a[5], 
+                "lateral": a[5],
                 "forward": a[4],
-                "vertical":a[2],
+                "vertical": a[2],
                 "fire1": True,
                 "fire2": True,
-                "end": True}, frame
+                "end": True,
+            }, frame
 
-
-        a = 18*[1500]
-
+        a = 18 * [1500]
 
         # video is 480 by 640, at end we want the point approx. (210, 350)
         self.frame = frame
-        circles = self.get_circles()
-
+        circles = self.get_circles(frame)
 
         # Image processing
-        if circles is not None:             # Circles detected
+        if circles is not None:  # Circles detected
             logging.info("Circle detected")
 
             self.lostSight = 0
 
             # Get distance with ping 360
             obstacles = self.p.get_obstacles()
-            if (obstacles is not None):
+
+            if obstacles is not None:
                 # Sort obstacles by size
-                sorted_obstacles = sorted(
-                    obstacles, 
-                    key=lambda x: x.area, 
-                    reverse=True)
-                
+                sorted_obstacles = sorted(obstacles, key=lambda x: x.area, reverse=True)
+
                 # Grab the distance of the first object in the list
                 largest_object = sorted_obstacles[0]
                 self.distance_to_target = largest_object.distance
                 logging.info("Distance: " + self.distance_to_target)
 
-
-                if(self.distance_to_target < self.far_near_boundary):
+                if self.distance_to_target < self.far_near_boundary:
                     self.near = True
                     logging.info("Near")
 
             # Center of largest circle - aim for this
-            x,y = circles[0,0], circles[0,1]
+            x, y = circles[0, 0], circles[0, 1]
 
-
-            if(self.distance_to_target < self.fire_distance):
-
-                if (not self.fired_torpedo_1):
+            if self.distance_to_target < self.fire_distance:
+                if not self.fired_torpedo_1:
                     a[2] = 1440
                     logging.info("Fire torpedo 1!!!")
                     self.fired_torpedo_1 = True
                     return {
-                        "lateral": a[5], 
+                        "lateral": a[5],
                         "forward": a[4],
-                        "vertical":a[2],
+                        "vertical": a[2],
                         "fire1": True,
                         "fire2": False,
-                        "end": False}, frame
+                        "end": False,
+                    }, frame
                 else:
                     logging.info("Fire torpedo 2!!!")
                     self.fired_torpedo_2 = True
                     return {
-                        "lateral": a[5], 
+                        "lateral": a[5],
                         "forward": a[4],
-                        "vertical":a[2],
+                        "vertical": a[2],
                         "fire1": True,
                         "fire2": True,
-                        "end": False}, frame
+                        "end": False,
+                    }, frame
 
-
-            if(not self.near):
+            if not self.near:
                 # Aligning from afar
 
                 # X alignment
-                if(self.center_x < x + self.threshold_far):       # Strafe Left
+                if self.center_x < x + self.threshold_far:  # Strafe Left
                     a[5] = 1480
                     logging.info("Left")
-                if(self.center_x > x + self.threshold_far):       # Strafe Right
-                    a[5] = 1520 
+                if self.center_x > x + self.threshold_far:  # Strafe Right
+                    a[5] = 1520
                     logging.info("Right")
 
                 # Y alignment
-                if(self.center_y < y + self.threshold_far):       # Dive
+                if self.center_y < y + self.threshold_far:  # Dive
                     a[2] = 1520
                     logging.info("Dive")
-                if(self.center_y > y + self.threshold_far):       # Ascend
-                    a[2] = 1480 
+                if self.center_y > y + self.threshold_far:  # Ascend
+                    a[2] = 1480
                     logging.info("Ascend")
 
-                # Print motors and return commands    
+                # Print motors and return commands
                 print(a)
-                return {
-                    "lateral": a[5], 
-                    "forward": a[4],
-                    "vertical":a[2], 
-                    "end": False}, frame
+                return {"lateral": a[5], "forward": a[4], "vertical": a[2], "end": False}, frame
 
-            if(self.near):
+            else:
                 # Aligning near the target
                 # X alignment
-                if(self.target_center_x < x + self.threshold_near):  # Strafe Left
+                if self.target_center_x < x + self.threshold_near:  # Strafe Left
                     a[5] = 1480
                     logging.info("Left")
-                if(self.target_center_x > x + self.threshold_near):  # Strafe Right
-                    a[5] = 1520 
+                if self.target_center_x > x + self.threshold_near:  # Strafe Right
+                    a[5] = 1520
                     logging.info("Right")
 
                 # Y alignment
-                if(self.target_center_y < y + self.threshold_near):  # Dive
+                if self.target_center_y < y + self.threshold_near:  # Dive
                     a[2] = 1520
                     logging.info("Dive")
-                if(self.target_center_y > y + self.threshold_near):  # Ascend
-                    a[2] = 1480 
+                if self.target_center_y > y + self.threshold_near:  # Ascend
+                    a[2] = 1480
                     logging.info("Ascend")
 
-                # Print motors and return commands    
+                # Print motors and return commands
                 print(a)
-                return {
-                    "lateral": a[5], 
-                    "forward": a[4],
-                    "vertical":a[2], 
-                    "end": False}, frame
-                
+                return {"lateral": a[5], "forward": a[4], "vertical": a[2], "end": False}, frame
 
-
-
-        else:   # No circles detected
+        else:  # No circles detected
             # Potentially can resort to scanning sonar with 180 degree sweep
             # and find if the object is on the left or right of the sub
             self.lostSight += 1
 
             if self.lostSight > 300:
                 # Target has been lost for too long and mission needs to terminate
-                return {
-                    "lateral": 0, 
-                    "forward": 0, 
-                    "vertical": 0, 
-                    "end": True}, self.frame
-
+                return {"lateral": 0, "forward": 0, "vertical": 0, "end": True}, self.frame
 
             elif self.lostSight > 3000:
                 # No circles detected and need to back up looking any
-                a[4]=1450
-                return {
-                    "lateral": 0, 
-                    "forward": a[4], 
-                    "vertical": 0, 
-                    "end": False}, self.frame
+                a[4] = 1450
+                return {"lateral": 0, "forward": a[4], "vertical": 0, "end": False}, self.frame
             else:
                 # Lost image for a short duration so waiting to see
                 # if the image will detect another circle
-                return {
-                    "lateral": 0, 
-                    "forward": 0, 
-                    "vertical": 0, 
-                    "end": False}, self.frame
+                return {"lateral": 0, "forward": 0, "vertical": 0, "end": False}, self.frame
 
-            
-
-
-
-
-
-
-        #return {"lateral": 0, "forward": 0, "vertical": 0, "end": False}, self.frame
+        # return {"lateral": 0, "forward": 0, "vertical": 0, "end": False}, self.frame
 
 
 if __name__ == "__main__":
