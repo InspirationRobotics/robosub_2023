27aa89d229cb322b0479c82585147795ee9491d1
unknown <saxenaparth007@gmail.com>
2023-08-02 09:32:04 -0700

    init node in mission

--- missions/coin_toss_gate.py
+++ missions/coin_toss_gate.py
@@ -1,6 +1,8 @@
 from auv.mission import gate_mission, cointoss_mission, style_mission
 from auv.utils import arm, disarm, deviceHelper
 
+import rospy
+rospy.init_node("initial mission", anonymous=True)
 # load sub config
 config = deviceHelper.variables
 arm.arm()
