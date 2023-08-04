1a353787fe60689e8d7bfcbe6fdc495cb38494a5
Dat-Bois <eeshvij@gmail.com>
2023-08-04 08:15:22 -0700

    disarm script

--- auv/utils/gShutdown.py
+++ auv/utils/gShutdown.py
@@ -2,27 +2,49 @@ import Jetson.GPIO as GPIO
 import sys
 import time
 import os
-from .disarm import disarm
+from auv.utils.disarm import disarm
+from auv.utils.deviceHelper import variables
+import signal
 
-killPin = 35 #19 on onyx
+
+if (variables.get("sub", "onyx") == "onyx"):
+    killPin = 19
+else:
+    killPin=35
 global state
+global loop
 startTime = 0
 state = True
+loop = True
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(killPin, GPIO.IN)
 
-while True:
+
+def onExit(signum, frame):
+    global loop
+    try:
+        loop = False
+        print("\n\nCleanly Exited")
+        exit(1)
+    except:
+        pass
+
+signal.signal(signal.SIGINT, onExit)
+
+while loop:
     current = GPIO.input(killPin)
     if current == 1:
         state = True
         startTime = time.time()
 
-    if current == 0 and state == False:
+    if current == 0 and state == True and startTime!=0:
         if time.time() - startTime > 1:
             print("Disarming")
             disarm()
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(killPin, GPIO.IN)
             state = False
-    print(GPIO.input(killPin))
+    #print(GPIO.input(killPin))
     time.sleep(0.1)
