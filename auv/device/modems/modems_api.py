a806385b85299bb4ff33a16ecfea41a4f86d9e76
Dat-Bois <eeshvij@gmail.com>
2023-08-03 20:30:38 -0700

    fixing ack ?

--- auv/device/modems/modems_api.py
+++ auv/device/modems/modems_api.py
@@ -25,6 +25,7 @@ class Modem:
         self.ACK = 0
         self.blocked = False
         self.in_transit = []  # [message, time_sent, ack-expected, modemaddr, priority]
+        self.ack_received = []
 
         self.modemAddr = None
         self.voltage = None
@@ -147,6 +148,7 @@ class Modem:
     def _send_loop(self):
         while self.sending_active:
             to_remove = []
+            self.ack_received = []
 
             for it, packet in enumerate(self.in_transit):
                 msg, time_sent, time_last_sent, ack, dest_addr, priority = packet
@@ -162,16 +164,17 @@ class Modem:
                     continue
 
                 # retry if no ack received after 3 seconds
-                if time.time() - time_last_sent > 3.0:
+                if time.time() - time_last_sent > 3.0 and ack not in self.ack_received:
                     print(f'[DEBUG] Retrying message "{msg}"')
                     ret = self._transmit(msg, ack=ack, dest_addr=dest_addr)
                     packet[2] = ret
 
-                    # give some time for other threads to be able to send ack
-                    time.sleep(0.05)
+                # print(self.ack_received)
+            
+            time.sleep(0.1)
 
-            # remove timed out messages
-            self.in_transit = [packet for it, packet in enumerate(self.in_transit) if it not in to_remove]
+            # remove timed out messages and received acks
+            self.in_transit = [packet for it, packet in enumerate(self.in_transit) if it not in to_remove and packet[3] not in self.ack_received]
 
     def _receive_loop(self):
         data = ""
@@ -220,6 +223,8 @@ class Modem:
         # just after a message (ack of the message)
         ack = ack.replace("@", "")
         ack = int(ack)
+
+        print(ack)
         
         # print(ack, self.in_transit, expecting_ack)
         if expecting_ack:
@@ -230,7 +235,7 @@ class Modem:
         # remove the corresponding msg from in_transit
         for i, packet in enumerate(self.in_transit):
             if packet[3] == ack:
-                self.in_transit.pop(i)
+                self.ack_received.append(ack)
                 return
 
         print(f'[WARNING] Received ack: "{ack}" but no corresponding message found, maybe timed out?')
